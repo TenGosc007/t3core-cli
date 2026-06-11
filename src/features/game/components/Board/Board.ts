1366c7eeb038f4a5b7a5432772c8b1acc2e75919
Aleksander Atamanczuk <aleksander.atamanczuk@gmail.com>
2026-06-11 22:17:53 +0200

    refactor: move board border constants outside Board function

--- src/features/game/components/Board/Board.ts
+++ src/features/game/components/Board/Board.ts
@@ -4,14 +4,14 @@ import { getGame } from "@/features/game/services/gameSession";
 import { colorLabelSymbol } from "@/features/game/util/colorLabelSymbol";
 import { s } from "@/utils/styledLabel";
 
+const top = s.grey.bold("┌───┬───┬───┐");
+const mid = s.grey.bold("├───┼───┼───┤");
+const bot = s.grey.bold("└───┴───┴───┘");
+const col = s.grey.bold("│");
+
 export const Board = () => {
   const game = getGame();
   const fields = game.board;
-
-  const top = s.grey.bold("┌───┬───┬───┐");
-  const mid = s.grey.bold("├───┼───┼───┤");
-  const bot = s.grey.bold("└───┴───┴───┘");
-  const col = s.grey.bold("│");
 
   console.log(top);
   fields.forEach((i: BoardField, idx: number) => {
