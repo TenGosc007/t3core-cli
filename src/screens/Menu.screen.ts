0a5c67d93480f0fec3b03698006e79ede6c7e043
Aleksander Atamanczuk <aleksander.atamanczuk@gmail.com>
2026-06-24 13:46:25 +0200

    refactor: remove redundant screen wrappers and export views directly from feature modules

--- src/screens/Menu.screen.ts
+++
@@ -1,7 +0,0 @@
-import type { AppRoute } from "../navigation/routes";
-
-import { MenuView } from "../features/menu/menu";
-
-export const MenuScreen = async (): Promise<AppRoute> => {
-  return MenuView();
-};
--- src/screens/Settings.screen.ts
+++
@@ -1,7 +0,0 @@
-import type { AppRoute } from "../navigation/routes";
-
-import { SettingsView } from "../features/settings/settings";
-
-export const SettingsScreen = async (): Promise<AppRoute> => {
-  return SettingsView();
-};
