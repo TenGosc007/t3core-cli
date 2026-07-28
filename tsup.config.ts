1beea12c3a1c1cf6c5f42f322de1c881f228a669
Aleksander Atamanczuk <aleksander.atamanczuk@gmail.com>
2026-07-28 19:18:48 +0200

    refactor: update tsup config target to node20 and enable treeshaking

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["index.ts"],
   format: ["esm"],
-  target: "node16",
+  target: "node20",
   clean: true,
   dts: false,
   minify: true,
   splitting: false,
   sourcemap: false,
+  treeshake: true,
   loader: { ".tsx": "tsx" },
   external: ["conf"],
 });
